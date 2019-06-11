d9cf6d3fad0b8f7cc1be28310751d9d26d3bbd16
mindeby <debora.marisa.ribeiro@gmail.com>
2019-06-11 18:26:07 +0200

    got 12 users

--- js/main.js
+++ js/main.js
@@ -1,29 +1,60 @@
 document.addEventListener('DOMContentLoaded', () => { //wait until everything loaded
 
-  const test = document.getElementById('test');
-
-
+  const cards = document.getElementsByClassName("card"); //array of card li's
+  const names = document.getElementsByClassName("name");
+  const users = [];
 
   function fetchData(url){
     return fetch(url)
-      .then(res => res.json()) //simplify code, reusability
-      .catch(error => console.log('It seems we have a problem', error)) //error handling
-  }
+      .then(res => res.json())
+      .catch(error => console.log('It seems we have a problem with the server', error)) //error handling
+  } //simplify code, reusability, convert data to json
 
-function printImage(data){
-  test.innerHTML += '<img src="' + data + '">';
+  function getUsers(){ //get 12 users
+    for (let i = 0; i<=12; i += 1) {
+      Promise.all([
+      fetch('https://randomuser.me/api/?results=12&inc=name, picture, email, location, login, phone, dob &noinfo &nat=US')
+      ])
+      return fetchData('https://randomuser.me/api/?results=12&inc=name, picture, email, location, login, phone, dob &noinfo &nat=US')
+      .then(data=> users.push((data.results)))
+    }
+  }
+  getUsers(); //got 12 users and added them to the users array
+
+function printImage(users, arr){ //adds profile pictures to every card
+  for (let i = 0; i< arr.length; i += 1) {
+    arr[i].innerHTML += '<img src="' + users[i].picture.large + '">';
+  }
 }
 
-  fetch('https://randomuser.me/api/')
-  return fetchData('https://randomuser.me/api/')
-  .then(data => printImage(data.results[0].picture.large))  //get access to the data in the api
+console.log(users);
+
+
+//printImage(users, cards);
+
+function printName(data, arr){ //adds profile pictures to every card
+  for (let i = 0; i< arr.length; i += 1) {
+    arr[i].textContent = data;
+  }
+}
+
+
+/*
+fetch('https://randomuser.me/api/?results=12&inc=name, picture, email, location, login, phone, dob &noinfo &nat=US')
+return fetchData('https://randomuser.me/api/?results=12&inc=name, picture, email, location, login, phone, dob &noinfo &nat=US')
+.then(data => printImage(data.results[0].picture.large, cards))
+*/
 
 
 
 
 
+/*
+  function getRandomPhraseAsArray(arr) { //get a random phrase & split it
+      const randomPhrase = arr[Math.floor(Math.random()* arr.length)];
+    }
 
-
+*/
 
 
 
